1185fc2d0605a32f3c73a647f4bf9adcca2deb91
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-13 19:40:53 -0300

    servlet de roles listo con setup

---
+++ src/main/java/com/modulocinco/controlador/RoleServlet.java
@@ -0,0 +1,42 @@
+package com.modulocinco.controlador;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.modulocinco.modelo.Role;
+
+/**
+ * Servlet implementation class RoleServlet
+ */
+@WebServlet("/RoleServlet")
+public class RoleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private List<Role> roles = new ArrayList<>();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RoleServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
