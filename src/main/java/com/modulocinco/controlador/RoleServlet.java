aefb31906457f61782e4bba27a33bde593964a88
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-13 19:44:53 -0300

    antes de completar el servlet

--- src/main/java/com/modulocinco/controlador/RoleServlet.java
+++ src/main/java/com/modulocinco/controlador/RoleServlet.java
@@ -31,12 +31,27 @@ public class RoleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Usamos el mismo esquema de definir acciones
+		String accion = request.getParameter("accion");
+		if(accion.equals("listar")){
+
+		} else {
+
+		}
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String nombre = request.getParameter("nombre");
+		roles.add(new Role(nombre));
+
+		
+
 	}
 
 }
